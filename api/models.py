6f584306775d9a5fc50e30a2e90cd31dd4f70a84
bliti <pablo.rivera.programmer@gmail.com>
2013-10-27 16:33:33 -0400

    models done and tested

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from uuid import uuid1
 
-# Create your models here.
+
+class User(models.Model):
+
+
+    email = models.EmailField(unique=True)
+    user_token = models.TextField(default=uuid1())
+    access_token = models.TextField(default=uuid1())
+
+
+class Message(models.Model):
+
+
+    sender = models.ForeignKey('User')
+    receiver = models.OneToOneField('User', related_name='message_reciever')
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
--- website/views.py
+++ website/views.py
@@ -1,1 +1,17 @@
 # Create your views here.
+from django.shortcuts import render
+
+
+def index(request):
+    """index page"""
+    return render(request, 'index.html')
+
+
+def signup(request):
+    """process signup"""
+    pass
+    
+
+def reset(request):
+    """reset api credentials"""
+    pass
