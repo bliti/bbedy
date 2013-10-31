05f85de91e9bcd3f7f3fbf76e0ea7f2c66d74bb7
bliti <pablo.rivera.programmer@gmail.com>
2013-10-31 15:48:52 -0400

    working out urls for website

--- website/urls.py
+++ website/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('website.views',
     url(r'^$', 'index'),
-    url(r'^//', 'send'),
+    url(r'^/reset/', 'reset'),
+    url(r'^/signup/', 'signup'),
+    url(r'^//', 'reset'),
+    
 )
